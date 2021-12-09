323a20ee38e9adad52cb04fe1fd14e14b11b0e9a
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2021-12-09 23:48:05 +0900

    Fix pukiwiki-vim

--- .config/fish/functions/pukiwiki-vim.fish
+++ .config/fish/functions/pukiwiki-vim.fish
@@ -1,3 +1,7 @@
+function open_vim
+	vim -c"e ++enc=euc-jisx0213" $argv
+end
+
 function pukiwiki-vim
 	if [ (count $argv) -lt 1 ]
 		echo "Usage: pukiwiki-vim [page name]"
@@ -5,9 +9,9 @@ function pukiwiki-vim
 	end
 	set filename (echo -n $argv[1] | iconv -t EUC-JISX0213 | xxd -p | tr -d '\n' | tr [:lower:] [:upper:]).txt
 	if [ -e $PWD/$filename ]
-		vim $filename
-	elif [ -e $PWD/wiki/$filename ]
-		vim wiki/$filename
+		open_vim $filename
+	else if [ -e $PWD/wiki/$filename ]
+		open_vim wiki/$filename
 	else
 		echo "File not found, expected "$filename
 	end
