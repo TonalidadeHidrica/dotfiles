1a202495d62b3737f43a16718ba22ff19aa634a5
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2021-12-30 03:43:47 +0900

    Tried to implement editorial-vim with fish script

--- .config/fish/functions/editorial-vim.fish
+++ .config/fish/functions/editorial-vim.fish
@@ -1,4 +1,20 @@
 function editorial-vim
+	if ! type -q tomlq
+		echo "Error: tomlq is required for the script, but it is not installed, or not in the PATH."
+		echo "You should install it by: cargo install tomlq"
+		return 1
+	end
+	set toml_file translation.toml
+	set id $argv[1]
+	set file_orig {$id}.md
+	if [ ! -e $file_orig ]
+		echo "Error: "$file_orig" does not exist."
+		return 1
+	end
+	if [ -e $toml_file ]
+	else
+		echo "Warning: "$toml_file" was not found."
+	end
 	vim -O -c"call EditorialMode()" {$argv[1]}en.md $argv[1].md
 end
 
