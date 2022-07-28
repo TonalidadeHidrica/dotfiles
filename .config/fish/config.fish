cbe9d5938315ba939c910009fd18bf3ebd072ab1
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2022-07-28 20:33:18 +0900

    Add n setup line (may be deleted someday)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -49,3 +49,5 @@ end
 if [ -e /Users/seasellsheshell/.phpbrew/phpbrew.fish ]
 	source /Users/seasellsheshell/.phpbrew/phpbrew.fish
 end
+
+set -x N_PREFIX "$HOME/n"; contains "$N_PREFIX/bin" $PATH; or set -a PATH "$N_PREFIX/bin"  # Added by n-install (see http://git.io/n-install-repo).
