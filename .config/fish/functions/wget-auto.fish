8f3e40abfcd42e4d1b43c2a79ffee2aefa9cc8b0
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2022-08-02 19:30:31 +0900

    Add wget-auto

---
+++ .config/fish/functions/wget-auto.fish
@@ -0,0 +1,15 @@
+function wget-auto
+	if [ (count $argv) != 1 ]
+		echo "Usage: wget [URL]" 1>&2
+		return 1
+	end
+	set url $argv[1]
+	set domain (echo $url | sed -e 's/[^/]*\/\/\([^@]*@\)\?\([^:/]*\).*/\2/')
+	set domains (string split '.' $domain)
+	set reverse_domain (string join '.' $domains[-1..1])
+
+	cd ~/dl
+	mkdir -p $reverse_domain
+	cd $reverse_domain
+	wget $url
+end
