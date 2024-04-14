579694c815946a0629efa31924333d7f2a135263
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2024-04-14 17:45:21 +0900

    pwsh from tmux: cd to current directory (Windows side)

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,3 +4,57 @@ refreshenv
 Set-Alias -Name vim -Value nvim
 
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+Remove-Alias -Force -Name cd
+function cd {
+    param (
+        [string]$Path
+    )
+
+    # Check if $Path is empty, if so, assign $USERPROFILE
+    if ([string]::IsNullOrEmpty($Path)) {
+        $Path = $env:USERPROFILE
+    }
+
+    # Get the WSL2 PID
+    $wsl2_pid = $env:WSL2_CURRENT_PID
+
+    # Check if the WSL2 PID environment variable is set
+    if ($wsl2_pid -ne $null) {
+        # Define the path to the file
+        $file_path = Join-Path -Path $env:USERPROFILE -ChildPath ".powershell-cd\$wsl2_pid"
+
+        # Check if the directory exists, if not, create it
+        if (!(Test-Path -Path $file_path)) {
+            New-Item -Path $file_path -ItemType File -Force > $null
+        }
+
+        # Convert $Path to absolute path
+        $absolute_path = Convert-Path -Path $Path
+
+        # Append the destination to the file
+        Set-Content -Path $file_path -Value $absolute_path
+    }
+
+    # Call the original cd command to change directory
+    Set-Location $Path
+}
+
+# cd to current directory to store current path
+cd .
+
+# When started from WSL2, cd to home directory
+if ($env:WSL2_CURRENT_PID -ne $null) {
+    $directoryToCd = $env:USERPROFILE
+}
+# However, when spawned from powershell ran by tmux, cd to that directory
+if ($env:WSL2_PARENT_PID -ne $null) {
+    $directoryFile = "$env:USERPROFILE\.powershell-cd\$env:WSL2_PARENT_PID"
+    if (Test-Path $directoryFile) {
+        $directoryToCd = Get-Content $directoryFile
+    }
+}
+# Actual cd
+if ($directoryToCd -ne $null) {
+    cd $directoryToCd
+}
